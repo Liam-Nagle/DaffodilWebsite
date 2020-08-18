02a262100d6c8dcf2e6d6dd6a75c2054628eb955
Liam Nagle <liamnagle5@gmail.com>
2020-08-18 23:21:55 +0100

    Testing adding animated search bar. Need to work out styling and positioning

---
+++ javascript/search.js
@@ -0,0 +1,8 @@
+$('.topnav').on('click', '.search-toggle', function(e) {
+  var selector = $(this).data('selector');
+
+  $(selector).toggleClass('show').find('.search-input').focus();
+  $(this).toggleClass('active');
+
+  e.preventDefault();
+});
