53943d63a209dc8bd234398de2d5d24ba355127b
Liam Nagle <liamnagle5@gmail.com>
2021-02-27 00:11:58 +0000

    Updated mobile stying. Need to center shopping cart on right hand side properly.

--- javascript/quantity.js
+++ javascript/quantity.js
@@ -5,15 +5,13 @@ var quantity = document.getElementById("quantity").value;
 function QuantityPlus() {
 	quantity = parseInt(quantity) + 1;
 	document.getElementById("quantity").value = quantity;
-	document.getElementById("quantity-negative").style.visibility = "hidden";
 }
 
 function QuantityMinus() {
 	if(quantity <= 1) {
-		document.getElementById("quantity-negative").style.visibility = "visible";
+		quantity = parseInt(quantity);
 	}else {
-		quantity = parseInt(quantity) -1;
+		quantity = parseInt(quantity) - 1;
 		document.getElementById("quantity").value = quantity;
-		document.getElementById("quantity-negative").style.visibility = "hidden";
 	}
 }
